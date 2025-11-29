e3ce29d52f8ba52d14cb27e9e146c86f5b2a3ac6
protwocol <anonymicease@gmail.com>
2025-11-30 03:07:04 +0800

    feat: complete calculator

--- foundations/projects/odin-calculator/script.js
+++ foundations/projects/odin-calculator/script.js
@@ -1,9 +1,6 @@
 // variables
 let history = null;
 let current = 0;
-let operandA = 0;
-let operandB = 0;
-let operator = "";
 const operators = /[+\-*/]/;
 
 // elements
@@ -15,55 +12,147 @@ const historyValue = document.querySelector("#history");
 button.addEventListener("click", (e) => {
   const value = e.target;
   current = value.value;
+
+  // operand keys
   if (value.matches(".buttons__item--operand")) {
     if (currentValue.textContent === "0") {
       currentValue.textContent = current;
     } else {
       currentValue.textContent += current;
     }
-  } else if (value.matches(".buttons__item--operator")) {
-    operator = current;
-    // check for consecutive decimals
+    return;
+  }
+
+  // operator keys
+  if (value.matches(".buttons__item--operator")) {
+    // check for consecutive operator
+    if (checkConsecutiveOperators(currentValue.textContent)) {
+      if (operators.test(current)) {
+        const operator = currentValue.textContent.match(operators);
+        const parts = currentValue.textContent.split(operators);
+        if (parts[1] === "") {
+          return;
+        } else {
+          operate(parts, operator[0]);
+          return;
+        }
+      }
+      const parts = currentValue.textContent.split(operators);
+      console.log(parts);
+      if (parts[1] === "") {
+        currentValue.textContent += current;
+        return;
+      } else {
+        console.log(parts[1]);
+        if (checkConsecutiveDecimal(parts[1])) {
+          return;
+        } else {
+          currentValue.textContent += current;
+          return;
+        }
+      }
+    }
     if (checkConsecutiveDecimal(currentValue.textContent)) {
+      if (current === ".") {
+        return;
+      }
+    }
+    currentValue.textContent += current;
+    return;
+  }
+
+  // delete key
+  if (value.matches("#delete")) {
+    if (currentValue.textContent.length === 1) {
+      currentValue.textContent = "0";
       return;
     }
-    // check for consecutive operators
-    // if (checkConsecutiveOperator(currentValue.textContent, operators)) {
-    //   currentValue.textContent.slice(currentValue.textContent.length - 1, -1);
-    //   currentValue.textContent += operator;
-    //   return;
-    // } else {
-    //   console.log(operator);
-    //   currentValue.textContent += operator;
-    // }
-    currentValue.textContent += current;
+    currentValue.textContent = currentValue.textContent.slice(0, -1);
+    return;
+  }
+
+  // clear key
+  if (value.matches("#clear")) {
+    currentValue.textContent = "0";
+    historyValue.textContent = "";
+    return;
+  }
+
+  // equals
+  if (value.matches("#equals")) {
+    const operator = currentValue.textContent.match(operators);
+    const parts = currentValue.textContent.split(operators);
+    if (parts[1] === "") {
+      return;
+    }
+    if (parts.length === 1) {
+      return;
+    }
+    operate(parts, operator[0]);
+    return;
   }
 });
 
-// dot
+// check consecutive operator
+function checkConsecutiveOperators(value) {
+  // check for consecutive operators
+  if (operators.test(value)) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+// check consecutive decimal
 function checkConsecutiveDecimal(value) {
   return value.includes(".");
 }
 
-function checkConsecutiveOperator(math, charsToFind) {
-  return charsToFind.test(math);
-}
+// --------------- operations
 
-// operator functions
+// addition
 function add(a, b) {
-  return a + b;
+  let sum = a + b;
+  historyValue.textContent = currentValue.textContent;
+  currentValue.textContent = parseFloat(sum.toFixed(1));
 }
 
+// subtraction
 function subtract(a, b) {
-  return a - b;
+  let difference = a - b;
+  historyValue.textContent = currentValue.textContent;
+  currentValue.textContent = parseFloat(difference.toFixed(1));
 }
 
+// multiplication
 function multiply(a, b) {
-  return a * b;
+  let product = a * b;
+  historyValue.textContent = currentValue.textContent;
+  currentValue.textContent = parseFloat(product.toFixed(1));
 }
 
+// divition
 function divide(a, b) {
-  return a / b;
+  let quotient = a / b;
+  if (b === 0) {
+    alert("Error dividing with zero");
+    return;
+  }
+  historyValue.textContent = currentValue.textContent;
+  currentValue.textContent = parseFloat(quotient.toFixed(1));
 }
 
-function operate() {}
+// operate
+function operate(splited, operator) {
+  let operandA = parseFloat(splited[0]);
+  let operandB = parseFloat(splited[1]);
+  if (operator === "+") {
+    add(operandA, operandB);
+  } else if (operator === "-") {
+    subtract(operandA, operandB);
+  } else if (operator === "*") {
+    multiply(operandA, operandB);
+  } else {
+    divide(operandA, operandB);
+  }
+}
